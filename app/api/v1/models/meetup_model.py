dc7bc4355ff7a52ac78ae3f4b8b8354a0f078923
mr-pagani <brian.mwai346@gmail.com>
2019-01-11 12:07:13 +0300

    [Starts Feature #163073214] This uploads the view upcoming meetups endpoint

--- app/api/v1/models/meetup_model.py
+++ app/api/v1/models/meetup_model.py
@@ -1,3 +1,4 @@
+from datetime import date, time, datetime
 
 MEETUP_LIST = []
 
@@ -6,13 +7,13 @@ class Meetup():
     def create_meetup(self, meetup_id, createdOn, location, images, topic, happeningOn, tags):
         self.single_meet = {}
 
-        self.single_meet[meetup_id] = meetup_id
-        self.single_meet[createdOn] = createdOn
-        self.single_meet[location] = location
-        self.single_meet[images] = images
-        self.single_meet[topic] = topic
-        self.single_meet[happeningOn] = happeningOn
-        self.single_meet[tags] = tags
+        self.single_meet['meetup_id'] = meetup_id
+        self.single_meet['createdOn'] = createdOn
+        self.single_meet['location'] = location
+        self.single_meet['images'] = images
+        self.single_meet['topic'] = topic
+        self.single_meet['happeningOn'] = happeningOn
+        self.single_meet['tags'] = tags
 
         MEETUP_LIST.append(self.single_meet)
         return {
@@ -28,4 +29,18 @@ class Meetup():
             return {
                         "status": 200,
                         "data": MEETUP_LIST
+                        }
+
+    def get_upcoming(self):
+        upcoming = []
+        for meetup in MEETUP_LIST:
+            if datetime.strptime(meetup['happeningOn'], '%d %b %Y') > datetime.today():
+                upcoming.insert(0, meetup)
+        if len(upcoming) == 0 or len(MEETUP_LIST) == 0:
+            return {"message": "meetups not found"}
+
+        else:
+            return {
+                        "status": 200,
+                        "data": upcoming
                         }
