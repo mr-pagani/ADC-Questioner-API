9e389e46971ada2c9d5406975a71e866fdb0d12d
mr-pagani <brian.mwai346@gmail.com>
2019-01-16 17:37:23 +0300

    [Chore #163072364] added more endpoint tests

--- app/tests/v1/test_meetups.py
+++ app/tests/v1/test_meetups.py
@@ -23,5 +23,35 @@ class TestMeetups(BaseTestCase):
 
 
     def test_view_meetups(self):
-        response = self.client.get('/api/v1/meetups')
-        self.assertEqual(response.status_code, 200)
+        with self.client:
+            response = self.client.get('/api/v1/meetups')
+            x = json.loads(x.data)
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual("meetups not found", x["message"])
+
+    def test_get_meetup_by_id(self):
+        with self.client:
+            response = self.client.get('/api/v1/meetups/<meetup_id>')
+            x = json.loads(x.data)
+        self.assertEqual(response.status_code,404)
+        self.assertEqual("meetups not found", x["message"])
+
+    def test_view_upcoming_meetups(self):
+        with self.client:
+            response = self.client.get('/api/v1/meetups/<meetup_id>')
+            x = json.loads(x.data)
+        self.assertEqual(response.status_code,404)
+        self.assertEqual("meetups not found", x["message"])
+
+    def test_delete_meetup(self):
+        with self.client:
+            response = self.client.get('/api/v1/meetups/<meetup_id>')
+            x = json.loads(x.data)
+        self.assertEqual(response.status_code,404)
+        self.assertEqual("Meetup with the id provided was not found", x["error"])
+
+    def test_create_rsvp(self):
+        with self.client:
+            response = self.client.get('/api/v1/meetups/<meetup_id>')
+            x = json.loads(x.data)
+        self.assertEqual(response.status_code,201)
